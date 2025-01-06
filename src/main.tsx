3524e9e9143fcea80d02f6ce77a9fa554936c876
Ricardo Rodriguez <ricardorodriguez@GC00089.local>
2025-01-05 18:18:15 -0700

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-// import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./main/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-    <App />
-  // </StrictMode>,
-)
+// Create a client
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
+);
